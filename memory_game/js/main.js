b4d7096330e8e18f6fe989fe1bf991c089424c4d
David Pitz <dw_pitz@yahoo.com>
2019-09-05 14:00:24 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,23 +1,23 @@
-cards = ["queen", "queen", "king", "king"];
-cardsInPlay = [];
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-
-/*if(cardsInPlay.length === 2) {
-	//Code
-}*/
-
-if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
+function checkForMatch() {
+	if(cardsInPlay.length === 2) {
+		
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			console.log("You found a match!");
+		} else {
+			console.log("Sorry, try again.");
+		}
+	}
 }
 
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+flipCard(0);
+flipCard(2);
 
