34eca2e6f5a2388f64258e24c4b54053dac95516
David Pitz <dw_pitz@yahoo.com>
2019-09-06 23:32:15 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -27,8 +27,8 @@ var cards = [
 var cardsInPlay = [];
 
 function checkForMatch() {
-	if(cardsInPlay.length === 2) {
-		
+	//this.setAttribute('src', cards[cardId].cardImage)
+	if(cardsInPlay.length === 2) {	
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("You found a match!");
 		} else {
@@ -37,14 +37,25 @@ function checkForMatch() {
 	}
 }
 
-function flipCard(cardId) {
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage)
 	checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+	}
+
+createBoard();
 
